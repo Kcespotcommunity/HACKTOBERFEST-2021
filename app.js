008ea8028244aa5cf3a76ae37c07336250ec231b
KarthickRaja2002 <74195242+KarthickRaja2002@users.noreply.github.com>
2021-10-13 15:48:28 +0530

    Search bar and post field included

--- app.js
+++ app.js
@@ -52,9 +52,9 @@ app.get("/register",(req,res)=>{
        res.render("register");
 })
 
-// app.get("/",(req,res)=>{
-       
-// })
+app.get("/dashboard",(req,res)=>{
+       res.render("dashboard")
+})
 
 app.listen(port,()=>{
     console.log("Server connected on: " + port)
